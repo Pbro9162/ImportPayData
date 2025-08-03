c7e71b0c6034ba477e05d7c496d077dfb1822259
pbro9162 <bcathcpar103@gmail.com>
2025-08-03 13:50:00 -0400

    feat: edited CboSheet_SelectedIndexChanged() to fetch and display batch id in gridview using SQL function.

--- ImportPayData/Form1.cs
+++ ImportPayData/Form1.cs
@@ -51,40 +51,54 @@ namespace ImportPayData
 
         private void CboSheet_SelectedIndexChanged(object sender, EventArgs e)
         {
+            string connectionString = "Server=DESKTOP-SUAB0U9;Database=10009;User ID=Tester;Password=password1234;TrustServerCertificate=True;";
             DataTable dt = tableCollection[cboSheet.SelectedItem.ToString()];
             //dataGridView1.DataSource = dt;
             if (dt != null)
             {
-                
-                List<PRTransactionMaster> t = new List<PRTransactionMaster>();
-                for (int i = 0; i < dt.Rows.Count; i++)
+                //GET BATCHID, UNIQUE FOR EACH IMPORT
+                using (SqlConnection connection = new SqlConnection(connectionString))
                 {
-                    PRTransactionMaster tr = new PRTransactionMaster();
+                    connection.Open();
+                    SqlCommand c = new SqlCommand("SELECT dbo.ufn_GET_IMPORTBATCH_ID(@param)", connection);
+                    int id = 99998; // Default value, change to take user input later.
 
-                    tr.IDGLCompany = Convert.ToInt32(dt.Rows[i]["Company ID"]);
-                    tr.CGLCompanyID = dt.Rows[i]["cGLCompanyID"].ToString();
-                    tr.IDPREEmployee = Convert.ToInt32(dt.Rows[i]["Employee ID"]);
-                    tr.CCheckNumber = dt.Rows[i]["Check Number"].ToString();
-                    tr.DCheckDate = Convert.ToDateTime(dt.Rows[i]["Check Date"]);
-                    tr.DAccountingDate = Convert.ToDateTime(dt.Rows[i]["Accounting Date"]);
-                    tr.DPayPeriodStart = Convert.ToDateTime(dt.Rows[i]["Pay Period Start"]);
-                    tr.DPayPeriodEnd = Convert.ToDateTime(dt.Rows[i]["Pay Period End"]);
-                    tr.NGrossAmount = Convert.ToDecimal(dt.Rows[i]["Gross Amount"]);
-                    tr.NNetAmount = Convert.ToDecimal(dt.Rows[i]["Net Amount"]);
-                    tr.NRegularPay = Convert.ToDecimal(dt.Rows[i]["Regular Pay"]);
-                    tr.NOvertimePay = Convert.ToDecimal(dt.Rows[i]["Overtime Pay"]);
-                    tr.NTimeOffPay = Convert.ToDecimal(dt.Rows[i]["Time Off Pay"]);
-                    tr.NRegularHours = Convert.ToDecimal(dt.Rows[i]["Regular Hours"]);
-                    tr.NOvertimeHours = Convert.ToDecimal(dt.Rows[i]["Overtime Hours"]);
-                    tr.NTimeOffHours = Convert.ToDecimal(dt.Rows[i]["Time Off Hours"]);
-                    tr.NDoNotPayHours = Convert.ToDecimal(dt.Rows[i]["Do Not Pay Hours"]);
-                    tr.BPRModule = false; // Assuming this is a boolean, adjust as necessary
-                    tr.CImportBatchID = dt.Rows[i]["cImportBatchID"].ToString();
-                    // Add the transaction to the list
-                    t.Add(tr);
+                    c.Parameters.AddWithValue("@param", id);
+                    string batchIDresult = c.ExecuteScalar().ToString();
+
+
+
+                    List<PRTransactionMaster> t = new List<PRTransactionMaster>();
+                    for (int i = 0; i < dt.Rows.Count; i++)
+                    {
+                        PRTransactionMaster tr = new PRTransactionMaster();
+
+                        tr.IDGLCompany = Convert.ToInt32(dt.Rows[i]["Company ID"]);
+                        tr.CGLCompanyID = dt.Rows[i]["cGLCompanyID"].ToString();
+                        tr.IDPREEmployee = Convert.ToInt32(dt.Rows[i]["Employee ID"]);
+                        tr.CCheckNumber = dt.Rows[i]["Check Number"].ToString();
+                        tr.DCheckDate = Convert.ToDateTime(dt.Rows[i]["Check Date"]);
+                        tr.DAccountingDate = Convert.ToDateTime(dt.Rows[i]["Accounting Date"]);
+                        tr.DPayPeriodStart = Convert.ToDateTime(dt.Rows[i]["Pay Period Start"]);
+                        tr.DPayPeriodEnd = Convert.ToDateTime(dt.Rows[i]["Pay Period End"]);
+                        tr.NGrossAmount = Convert.ToDecimal(dt.Rows[i]["Gross Amount"]);
+                        tr.NNetAmount = Convert.ToDecimal(dt.Rows[i]["Net Amount"]);
+                        tr.NRegularPay = Convert.ToDecimal(dt.Rows[i]["Regular Pay"]);
+                        tr.NOvertimePay = Convert.ToDecimal(dt.Rows[i]["Overtime Pay"]);
+                        tr.NTimeOffPay = Convert.ToDecimal(dt.Rows[i]["Time Off Pay"]);
+                        tr.NRegularHours = Convert.ToDecimal(dt.Rows[i]["Regular Hours"]);
+                        tr.NOvertimeHours = Convert.ToDecimal(dt.Rows[i]["Overtime Hours"]);
+                        tr.NTimeOffHours = Convert.ToDecimal(dt.Rows[i]["Time Off Hours"]);
+                        tr.NDoNotPayHours = Convert.ToDecimal(dt.Rows[i]["Do Not Pay Hours"]);
+                        tr.BPRModule = false; // Assuming this is a boolean, adjust as necessary
+                        tr.CImportBatchID = batchIDresult;
+                        // Add the transaction to the list
+                        t.Add(tr);
+                    }
+
+                    pRTransactionMasterBindingSource.DataSource = t;
+
                 }
-
-                pRTransactionMasterBindingSource.DataSource = t;
             }
         }
 
@@ -92,7 +106,7 @@ namespace ImportPayData
         {
             try
             {
-                string connectionString = "Server=DESKTOP-SUAB0U9;Database=10009;User ID=Tester;Password=password1234;TrustServerCertificate=True;"; 
+                string connectionString = "Server=DESKTOP-SUAB0U9;Database=10009;User ID=Tester;Password=password1234;TrustServerCertificate=True;"; // Replace with your actual connection string
                 DapperPlusManager.Entity<PRTransactionMaster>().Table("PRTransactionMaster");
                 List<PRTransactionMaster> transactions = pRTransactionMasterBindingSource.DataSource as List<PRTransactionMaster>;
                 if (transactions != null)
